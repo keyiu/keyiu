5b5c331d72514d21552565764cb4d35c7bb99c53
吴伟杰 <wu.weijie.will@gmail.com>
2019-05-19 23:00:27 +0800

    修改文件结构

---
+++ app/configs/config.development.js
@@ -0,0 +1,11 @@
+export default {
+  db: {
+    database: 'waimai',
+    username: 'waimai',
+    password: 'bXluZXh0Y2xvdWQ=',
+    host: 'data.keyiu.com',
+    dialect: 'mysql',
+    dialectOptions: { decimalNumbers: true },
+  },
+  secret: "bXluZXh0Y2xvdWQ="
+};
