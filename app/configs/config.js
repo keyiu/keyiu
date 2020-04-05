76e3241108ee2283c3d65f3d2772df1f9801c294
wuweijie <wu.weijie@outlook.com>
2020-04-05 13:47:29 +0800

    delete

--- app/configs/config.js
+++
@@ -1,6 +0,0 @@
-import * as Sequelize from 'sequelize';
-
-const env = process.env.NODE_ENV || 'development';
-import db from  `./config.${env}`;
-
-export const sequelize = new Sequelize(db.database, db.username, db.password, db);
